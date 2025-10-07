4fc05c767422ff896ea9dedf230c08ffac9e4f50
Gloria Oloyede <oloyedegloria78@gmail.com>
2025-10-07 11:09:05 +0100

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const SHA256 = require('crypto-js/sha256');
+
+
 class Block {
-    constructor (index, timestamp, data, previousHash = '') {
+    constructor (index, timestamp, data,
+ previousHash = '') {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
@@ -8,7 +12,9 @@ class Block {
     }
     
 calculateHash () {
-    returnSHA256 (this.index + this.previousHash + this.timestamp + JSON.strigify(this.data)).tostringify();   
+    return SHA256 (this.index + this.previousHash
+ + this.timestamp + 
+JSON.stringify(this.data)).toString();   
    }
 }
 
@@ -20,24 +26,68 @@ class Blockchain {
     
     
     createGenesisblock (){
-      return new block(0, "01/01/2026", "Genesis block", "0");
-    
+      return new Block(0, "01/01/2026",
+ "Genesis block", "0");
+}    
  
-    getLatestblock () {
-      return this.chain {this.chain.length - 1};
+    getLatestblock() {
+      return this.chain [this.chain.length - 1];
     }
     
     
     addBlock(newBlock) {
-       newBlock.previousHash = this.getLatestblock().hash;
+       newBlock.previousHash =
+ this.getLatestblock().hash;
        newBlock.hash = newBlock.calculateHash();
        this.chain.push(newBlock);
     }
     
-    
+isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+        const currentBlock = this.chain[i];
+        const previousBlock = this.chain[i - 1];
+
+        
+        if (currentBlock.hash !== 
+currentBlock.calculateHash()) {
+            return false;
+        }
+
+        
+        if (currentBlock.previousHash !== 
+previousBlock.hash) {
+            return false;
+        }
+    }
+
+    return true;
+}
+}
+
 let riousCoin = new Blockchain();
-riousCoin.addBlock(new Block(1, "30/09/2025"().toString(), {amount: 4}));
-riousCoin.addBlock(new Block(1, "30/09/2025"().toString(), {amount: 10}));
+       riousCoin.addBlock(
+    new Block(
+riousCoin.chain.length,
+ "10/03/2026",
+{ amount: 4 }));
+
+       riousCoin.addBlock(
+   new Block(
+riousCoin.chain.length,
+ "12/03/2026",
+{ amount: 10 }));    
 
 
+console.log("Blockchain valid? " +
+riousCoin.isChainValid());
+riousCoin.chain[1].data = { amount: 100 };
+console.log("Blockchain valid? " +
+riousCoin.isChainValid());
+console.log("\nFinal Blockchain:");
 console.log(JSON.stringify(riousCoin, null, 4));
+
+
+
+
+
+
